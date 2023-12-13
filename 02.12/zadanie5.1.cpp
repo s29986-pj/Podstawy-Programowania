31feb97aa218c6ae2eed67ca4f520d8940b7b18f
s29986-pj <s29986@pjwstk.edu.pl>
2023-12-13 19:44:04 +0100

    Dodanie zadan

---
+++ 02.12/zadanie5.1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+	vector<int> wektor;
+	int n;
+
+	cout << "Podaj ilosc elementow: ";
+	cin >> n;
+
+	int liczba, max = 0, indeks;
+	for (int i = 0; i < n; i++) {
+		cout << "Podaj liczbe: ";
+		cin >> liczba;
+		wektor.push_back(liczba);
+		if (liczba > max) {
+			max = liczba;
+			indeks = i;
+		}
+	}
+
+	cout << "Wartosc maksymalna wektora: " << max << endl;
+	cout << "Numer indeksu wartosci maksymalnej: " << indeks << endl;
+
+
+	return 0;
+}
+
+// Napisz program, w którym :
+// a) użytkownik poda liczbę elementów wektora;
+// b) pobrane zostaną od użytkownika wszystkie elementy wektora;
+// c) wskazana zostanie wartość elementu maksymalnego wektora;
+// d) Wskazany zostanie numer indeksu elementu maksymalnego.
---
+++ 02.12/zadanie5.2.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+using namespace std;
+
+//funckja zamieniajaca elementy tablicy
+void zamiana(int tab[], int size) {
+	int temp, i=0;
+	while(i < size - 1){ //petla wykonuje sie dopoki poruszamy sie po zakresie tablicy
+		//zamiana elementow (biezacy z nastepnym)
+		temp = tab[i];
+		tab[i] = tab[i + 1];
+		tab[i + 1] = temp;
+		i += 2; //skok o 2 pomija zamienione elementy
+	}
+}
+
+int main() {
+
+	int n;
+
+	//pytanie uzytkownika o rozmiar tablicy
+	cout << "Podaj rozmiar tablicy: ";
+	cin >> n;	
+	
+	//inicjalizacja tablicy
+	int* liczby = new int[n]; 
+
+	//wprowadzanie danych do tablicy przez uzytkownika
+	cout << "Podaj elementy tablicy:" << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> liczby[i];
+	}
+
+	//wypisanie elementow tablicy przed zamiana
+	cout << "\nTablica przed zamiana:" << endl << endl;
+	for (int i = 0; i < n ; i++) {
+		cout << liczby[i] << "\t";
+		if (i % 2 != 0) cout << endl;
+	}
+
+	//zamiana elementow
+	zamiana(liczby, n);
+
+	//wypisanie elementow tablicy po zamianie
+	cout << endl << endl << "Tablica po zamianie: " << endl << endl;
+	for (int i = 0; i < n; i++) {
+		cout << liczby[i] << "\t";
+		if (i % 2 != 0) cout << endl;
+	}
+	cout << endl;
+
+	return 0;
+}
+
+// Napisz program, który umożliwia :
+// a) wczytanie aktualnego rozmiaru tablicy liczb całkowitych;
+// b) wczytanie elementów tablicy liczb całkowitych;
+// c) zamianę miejscami elementów tablicy, lezących po przeciwnej stronie prostej pionowej,
+// dzielącej tablicę na dwie równe części.
